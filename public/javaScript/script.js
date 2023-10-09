2058faf4099178faee9251e55ddabba50ffde8de
KiddMiguel <kidimbamiguel@gmail.com>
2023-10-09 15:53:43 +0200

    Fichier

---
+++ public/javaScript/script.js
@@ -0,0 +1,10 @@
+// Cookies
+const btnSuccess = document.querySelector('.btn-success');
+console.log(btnSuccess);
+
+const cookies = document.querySelector('.cookies');
+console.log(cookies);
+
+btnSuccess.addEventListener('click', function() {
+    cookies.style.opacity = "0";
+});
---
+++ readme.txt
@@ -0,0 +1,9 @@
+Pour les pages :
+- Login et Inscription
+- accueil
+- produit
+
+Pociles : Roboto
+
+Color : rgba(1,48,97,255);
+Color : rgba(36,163,255,255);
