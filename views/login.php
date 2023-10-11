69d7c1604dd14d613fc763855e87e31ec44279e9
Wloyer <113607334+Wloyer@users.noreply.github.com>
2023-10-11 14:57:28 +0200

    *

---
+++ views/login.php
@@ -0,0 +1,24 @@
+<div class="container">
+<h1>Inscriptions</h1>
+
+<form action="">
+  <label for=""></label>
+  <input type="text">
+  <label for=""></label>
+  <input type="text">
+  <label for=""></label>
+  <input type="text">
+  <label for=""></label>
+  <input type="text">
+  <label for=""></label>
+  <input type="text">
+  <label for=""></label>
+  <input type="text">
+  <label for=""></label>
+  <input type="text">
+  
+  <label for=""></label>
+  <input type="text">
+</form>
+
+</div>
