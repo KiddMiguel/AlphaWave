aca943bbdbd1d23504f92514b83670e6aa0e98ae
Wloyer <113607334+Wloyer@users.noreply.github.com>
2023-10-11 15:38:47 +0200

    login

--- views/login.php
+++ views/login.php
@@ -1,10 +1,15 @@
-<div class="container centered">
-<h1>Connection</h1>
-<form action="post">
-  <label for="email">email</label>
-  <input type="email" name="email">
-  <label for="Mot de passe">Mot de passe</label>
-  <input type="text" name="moDePasse">
-</form>
-
+<div class="text-center">
+  <h1 class="mb-4">Connection</h1>
+    <form action="post">
+      <div class="mb-3">
+        <label for="email">email</label>
+        <input type="email" name="email">
+      </div>
+      <div class="mb-3">
+        <label for="Mot de passe">Mot de passe</label>
+      </div>
+      <div class="mb-3">
+      <input type="text" name="moDePasse">
+      </div>
+    </form>
 </div>
