f88a4adc95e8f68669c28a6f80ba96200acd43a8
Wloyer <113607334+Wloyer@users.noreply.github.com>
2023-10-11 15:35:30 +0200

    inscriptions html

---
+++ views/login.php
@@ -0,0 +1,10 @@
+<div class="container centered">
+<h1>Connection</h1>
+<form action="post">
+  <label for="email">email</label>
+  <input type="email" name="email">
+  <label for="Mot de passe">Mot de passe</label>
+  <input type="text" name="moDePasse">
+</form>
+
+</div>
