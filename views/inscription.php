80f79e5bd08d73832f2128ac089b22e452afd8b9
Wloyer <113607334+Wloyer@users.noreply.github.com>
2023-10-11 15:13:31 +0200

    inscription

---
+++ views/inscription.php
@@ -0,0 +1,26 @@
+
+
+<div class="container centered">
+<h1>Inscriptions</h1>
+<form action="post">
+  <label for="nom">Nom</label>
+  <input type="text" name="nom">
+  <label for="prenom">Prenom</label>
+  <input type="text" name="prenom">
+  <label for="email">email</label>
+  <input type="email" name="email">
+  <label for="telephone">telephone</label>
+  <input type="int" name="telephone">
+  <label for="codePostale">code Postale</label>
+  <input type="int" name="codePostale">
+  <label for="ville">Ville</label>
+  <input type="text" name="ville">
+  <label for="Mot de passe">Mot de passe</label>
+  <input type="text" name="moDePasse">
+
+  <label for="image">Photo de profil</label>
+  <input type="img">
+</form>
+
+</div>
+
